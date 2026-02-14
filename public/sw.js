ac7827c2b9b7dcd9a4bb6a57723af03aae31371b
byron-003 <byronoyoo2030@gmail.com>
2026-02-15 00:57:58 +0300

    lll

--- public/sw.js
+++ public/sw.js
@@ -2,19 +2,15 @@ const CACHE_NAME = 'pro-trader-v1';
 const ASSETS = [
   '/',
   '/index.html',
-  '/manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://unpkg.com/lucide@latest'
-];
+  '/manifest.json'
+]; // Removed external CDNs from mandatory caching
 
-// Install Event
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Fetch Event (Required for Installability)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
