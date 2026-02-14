2e834c608802023206c97bff5df0c61190ae4b5e
byron-003 <byronoyoo2030@gmail.com>
2026-02-15 01:12:25 +0300

    o

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const APP_ID = process.env.APP_ID;
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-
+app.get(express.static(path.join(__dirname, 'public', 'favicon.png')));
 app.get('/login', (req, res) => {
     const derivLoginUrl = `https://oauth.deriv.com/oauth2/authorize?app_id=${APP_ID}&l=EN&brand=deriv`;
     res.redirect(derivLoginUrl);
