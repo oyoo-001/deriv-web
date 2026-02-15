8b0e30b77b3b353763a41de68003e82d33210738
byron-003 <byronoyoo2030@gmail.com>
2026-02-15 18:07:28 +0300

    kolop

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,38 @@
 const CACHE_NAME = 'pro-trader-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-]; // Removed external CDNs from mandatory caching
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/favicon.png',
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/lightweight-charts@4.1.1/dist/lightweight-charts.standalone.production.js',
+    'https://unpkg.com/lucide@latest'
+];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
