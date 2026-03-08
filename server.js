0f245f87d5f134a4cf12b857d95a4fce8459ba50
byron-003 <byronoyoo2030@gmail.com>
2026-03-08 21:36:57 +0300

    changes

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.get('/callback', (req, res) => {
 app.get('/config', (req, res) => {
     res.json({ appId: APP_ID });
 });
+app.get('/ping', (req, res) => {
+    res.status(200).json({ status: 'ok' });});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
